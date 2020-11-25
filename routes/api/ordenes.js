3d2dbf75bf7fbf2576355666e68f81c112fff0da
CourtoisDiego <dfrivera026@gmail.com>
2020-11-24 18:45:05 -0600

    Get by id

--- routes/api/ordenes.js
+++ routes/api/ordenes.js
@@ -6,13 +6,25 @@ const mdbOrdenesModel = new OrdenesModelClass();
 
 router.get('/all', async (req, res)=>{
     try{
-      const rslt = await mdbProductModel.getAll()
+      const rslt = await mdbOrdenesModel.getAll()
       res.status(200).json(rslt);
     }catch(ex){
       console.log(ex);
       res.status(500).json({"msg":"Algo Paso Mal."});
     }
  
+  });
+
+  router.get('/one/:id', async (req, res)=>{
+    try{
+      let { id } = req.params;
+      let oneDocument = await mdbOrdenesModel.getById(id);
+      res.status(200).json(oneDocument);
+    } catch(ex){
+      console.log(ex);
+      res.status(500).json({ "msg": "Algo Paso Mal." });
+    }
+  
   });
 
 module.exports = router;
